0c677ce6d139a9837514d9f6f79b7957c6c4e35d
lichuanle <3065981302@qq.com>
2025-10-07 11:12:28 +0800

    查看购物车业务代码开发

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -22,4 +22,6 @@ public interface ShoppingCartMapper {
     @Insert("insert into shopping_cart(name,image,user_id,dish_id,setmeal_id,dish_flavor,number,amount,create_time)" +
             "values (#{name},#{image},#{userId},#{dishId},#{setmealId},#{dishFlavor},#{number},#{amount},#{createTime})")
     void insert(ShoppingCart shoppingCart);
+
+
 }
