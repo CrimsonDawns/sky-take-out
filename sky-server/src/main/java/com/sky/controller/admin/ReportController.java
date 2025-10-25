777d522121709bb32367339a8e00f2c6c1478048
lichuanle <3065981302@qq.com>
2025-10-25 21:36:43 +0800

    导出excel文件业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 @RestController
@@ -86,5 +87,11 @@ public class ReportController {
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
         log.info("销量前十的商品{},{}", begin, end);
         return Result.success(reportService.getSalesTop10Statistics(begin, end));
+    }
+
+    @GetMapping("/export")
+    @ApiOperation("导出订单数据")
+    public void export(HttpServletResponse response) {
+        reportService.exportBusinessData(response);
     }
 }
